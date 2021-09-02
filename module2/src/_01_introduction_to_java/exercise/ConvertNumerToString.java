aaeaae3ad2f727d14fa05de53e67227d2f726e47
khaiphan101 <khaiphanngoc@gmail.com>
2021-09-02 21:18:01 +0700

    sua bt

--- module2/src/_01_introduction_to_java/exercise/ConvertNumerToString.java
+++ module2/src/_01_introduction_to_java/exercise/ConvertNumerToString.java
@@ -16,178 +16,251 @@ public class ConvertNumerToString {
         else if(n < 10){
             switch (n){
                 case 0:
-                    str = "Zero";break;
+                    str = "Zero";
+                    break;
                 case 1:
-                    str = "One";break;
+                    str = "One";
+                    break;
                 case 2:
-                    str = "Two";break;
+                    str = "Two";
+                    break;
                 case 3:
-                    str = "Three";break;
+                    str = "Three";
+                    break;
                 case 4:
-                    str = "Four";break;
+                    str = "Four";
+                    break;
                 case 5:
-                    str = "Five";break;
+                    str = "Five";
+                    break;
                 case 6:
-                    str = "Six";break;
+                    str = "Six";
+                    break;
                 case 7:
-                    str = "Seven";break;
+                    str = "Seven";
+                    break;
                 case 8:
-                    str = "Eight";break;
+                    str = "Eight";
+                    break;
                 case 9:
-                    str = "Nine";break;
+                    str = "Nine";
+                    break;
             }
         }
         else if(n < 20){
             switch (n){
                 case 10:
-                    str = "Ten";break;
+                    str = "Ten";
+                    break;
                 case 11:
-                    str = "Eleven";break;
+                    str = "Eleven";
+                    break;
                 case 12:
-                    str = "Twelve";break;
+                    str = "Twelve";
+                    break;
                 case 13:
-                    str = "Thirdteen";break;
+                    str = "Thirdteen";
+                    break;
                 case 14:
-                    str = "Fourteen";break;
+                    str = "Fourteen";
+                    break;
                 case 15:
-                    str = "Fifteen";break;
+                    str = "Fifteen";
+                    break;
                 case 16:
-                    str = "Sixteen";break;
+                    str = "Sixteen";
+                    break;
                 case 17:
-                    str = "Seventeen";break;
+                    str = "Seventeen";
+                    break;
                 case 18:
-                    str = "Eighteen";break;
+                    str = "Eighteen";
+                    break;
                 case 19:
-                    str = "Nineteen";break;
+                    str = "Nineteen";
+                    break;
             }
         }
 
         else if(n<100){
             switch (n/10){
                 case 2:
-                    str = "Twenty ";break;
+                    str = "Twenty ";
+                    break;
                 case 3:
-                    str = "Thirty ";break;
+                    str = "Thirty ";
+                    break;
                 case 4:
-                    str = "Fourty ";break;
+                    str = "Fourty ";
+                    break;
                 case 5:
-                    str = "Fifty ";break;
+                    str = "Fifty ";
+                    break;
                 case 6:
-                    str = "Sixty ";break;
+                    str = "Sixty ";
+                    break;
                 case 7:
-                    str = "Seventy ";break;
+                    str = "Seventy ";
+                    break;
                 case 8:
-                    str = "Eighty ";break;
+                    str = "Eighty ";
+                    break;
                 case 9:
-                    str = "Ninety ";break;
+                    str = "Ninety ";
+                    break;
             }
             switch (n%10){
                 case 0: break;
                 case 1:
-                    str += "one ";break;
+                    str += "one ";
+                    break;
                 case 2:
-                    str += "two ";break;
+                    str += "two ";
+                    break;
                 case 3:
-                    str += "three ";break;
+                    str += "three ";
+                    break;
                 case 4:
-                    str += "four ";break;
+                    str += "four ";
+                    break;
                 case 5:
-                    str += "five ";break;
+                    str += "five ";
+                    break;
                 case 6:
-                    str += "six ";break;
+                    str += "six ";
+                    break;
                 case 7:
-                    str += "seven ";break;
+                    str += "seven ";
+                    break;
                 case 8:
-                    str += "eight ";break;
+                    str += "eight ";
+                    break;
                 case 9:
-                    str += "nine ";break;
+                    str += "nine ";
+                    break;
             }
         }
 
         else if(n<1000){
             switch (n/100){
                 case 1:
-                    str = "One hundred ";break;
+                    str = "One hundred ";
+                    break;
                 case 2:
-                    str = "Two hundred ";break;
+                    str = "Two hundred ";
+                    break;
                 case 3:
-                    str = "Three hundred ";break;
+                    str = "Three hundred ";
+                    break;
                 case 4:
-                    str = "Four hundred ";break;
+                    str = "Four hundred ";
+                    break;
                 case 5:
-                    str = "Five hundred ";break;
+                    str = "Five hundred ";
+                    break;
                 case 6:
-                    str = "Six hundred ";break;
+                    str = "Six hundred ";
+                    break;
                 case 7:
-                    str = "Seven hundred ";break;
+                    str = "Seven hundred ";
+                    break;
                 case 8:
-                    str = "Eigh hundred ";break;
+                    str = "Eigh hundred ";
+                    break;
                 case 9:
-                    str = "Nine hundred ";break;
+                    str = "Nine hundred ";
+                    break;
             }
             switch ((n/10)%10){
                 case 0: break;
                 case 1:
                     switch (n%100){
                         case 10:
-                            str += "and en";break;
+                            str += "and en";
+                            break;
                         case 11:
-                            str += "and eleven";break;
+                            str += "and eleven";
+                            break;
                         case 12:
-                            str += "and twelve";break;
+                            str += "and twelve";
+                            break;
                         case 13:
-                            str += "and thirdteen";break;
+                            str += "and thirdteen";
+                            break;
                         case 14:
-                            str += "and fourteen";break;
+                            str += "and fourteen";
+                            break;
                         case 15:
-                            str += "and fifteen";break;
+                            str += "and fifteen";
+                            break;
                         case 16:
-                            str += "and sixteen";break;
+                            str += "and sixteen";
+                            break;
                         case 17:
-                            str += "and seventeen";break;
+                            str += "and seventeen";
+                            break;
                         case 18:
-                            str += "and eighteen";break;
+                            str += "and eighteen";
+                            break;
                         case 19:
-                            str += "and nineteen";break;
+                            str += "and nineteen";
+                            break;
                     }
                     break;
                 case 2:
-                    str += "and twenty ";break;
+                    str += "and twenty ";
+                    break;
                 case 3:
-                    str += "and thirty ";break;
+                    str += "and thirty ";
+                    break;
                 case 4:
-                    str += "and fourty ";break;
+                    str += "and fourty ";
+                    break;
                 case 5:
-                    str += "and fifty ";break;
+                    str += "and fifty ";
+                    break;
                 case 6:
-                    str += "and sixty ";break;
+                    str += "and sixty ";
+                    break;
                 case 7:
-                    str += "and seventy ";break;
+                    str += "and seventy ";
+                    break;
                 case 8:
-                    str += "and eighty ";break;
+                    str += "and eighty ";
+                    break;
                 case 9:
-                    str += "and ninety ";break;
+                    str += "and ninety ";
+                    break;
             }
             switch ((n%100)%10){
                 case 0: break;
                 case 1:
-                    str += "one ";break;
+                    str += "one ";
+                    break;
                 case 2:
-                    str += "two ";break;
+                    str += "two ";
+                    break;
                 case 3:
-                    str += "three ";break;
+                    str += "three ";
+                    break;
                 case 4:
-                    str += "four ";break;
+                    str += "four ";
+                    break;
                 case 5:
-                    str += "five ";break;
+                    str += "five ";
+                    break;
                 case 6:
-                    str += "six ";break;
+                    str += "six ";
+                    break;
                 case 7:
-                    str += "seven ";break;
+                    str += "seven ";
+                    break;
                 case 8:
-                    str += "eigh ";break;
+                    str += "eigh ";
+                    break;
                 case 9:
-                    str += "nine ";break;
+                    str += "nine ";
+                    break;
             }
         }
         System.out.println(str);
