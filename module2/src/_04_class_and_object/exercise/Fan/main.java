db94ffd76c00d98ab11aba60401a60ddcc9dc47a
khaiphan101 <khaiphanngoc@gmail.com>
2021-09-07 21:10:43 +0700

    a

--- module2/src/_04_class_and_object/exercise/Fan/main.java
+++
@@ -1,8 +0,0 @@
-package _04_class_and_object.exercise.Fan;
-
-public class main {
-//    public static void main(String[] args) {
-//        Fan fan1 = new Fan(3, true, 10, "yellow");
-//        Fan fan1 = new Fan(2, false, 5, "blue");
-//    }
-}
