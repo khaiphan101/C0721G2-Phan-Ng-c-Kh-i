0d7e874b104d3d9a577e0a73790a97a2dd784cc0
khaiphan101 <khaiphanngoc@gmail.com>
2021-09-23 11:05:44 +0700

    sua bt

--- module2/src/_14_exception_and_debug/exercise/use_class_IllegalTriangleException/IllegalTriangleException.java
+++ module2/src/_14_exception_and_debug/exercise/use_class_IllegalTriangleException/IllegalTriangleException.java
@@ -1,10 +1,11 @@
 package _14_exception_and_debug.exercise.use_class_IllegalTriangleException;
 
-public class IllegalTriangleException extends Exception{
-    public IllegalTriangleException(){
+public class IllegalTriangleException extends Exception {
+    public IllegalTriangleException() {
 
     }
-    public IllegalTriangleException(String message){
+
+    public IllegalTriangleException(String message) {
         super(message);
     }
 }
--- module2/src/_14_exception_and_debug/exercise/use_class_IllegalTriangleException/Main.java
+++ module2/src/_14_exception_and_debug/exercise/use_class_IllegalTriangleException/Main.java
@@ -5,36 +5,36 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         int[] triangle;
         boolean flag = false;
-        while(!flag){
-            try{
+        while (!flag) {
+            try {
                 System.out.println("Nhập 3 cạnh tam giác");
                 triangle = creatTriangle();
                 System.out.println("triangle: " + Arrays.toString(triangle));
                 flag = true;
-            }
-            catch (IllegalTriangleException e) {
+            } catch (IllegalTriangleException e) {
                 System.err.println("Xảy ra ngoại lệ: " + e.getMessage());
-                System.err.println("Mời nhập lại" );
+                System.err.println("Mời nhập lại");
             }
         }
     }
-    public static int[] creatTriangle() throws IllegalTriangleException{
+
+    public static int[] creatTriangle() throws IllegalTriangleException {
         System.out.println("Enter a");
         int a = Integer.parseInt(sc.nextLine());
         System.out.println("Enter b");
         int b = Integer.parseInt(sc.nextLine());
         System.out.println("Enter c");
         int c = Integer.parseInt(sc.nextLine());
-        if(a<=0 || b <= 0 || c <=0){
+        if (a <= 0 || b <= 0 || c <= 0) {
             throw new IllegalTriangleException("Lỗi cạnh tam giác bé hơn 0");
-        }
-        else if(a+b<=c || b+c<=a || a+c<=b){
+        } else if (a + b <= c || b + c <= a || a + c <= b) {
             throw new IllegalTriangleException("Lỗi tổng 2 cạnh tam giác không lớn hơn cạnh còn lại");
         }
-        int[] triangle = {a,b,c};
+        int[] triangle = {a, b, c};
         return triangle;
     }
 }
