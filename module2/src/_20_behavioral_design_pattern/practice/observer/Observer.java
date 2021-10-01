fae29fab317e41279de6a22343db646e6a5b7918
khaiphan101 <khaiphanngoc@gmail.com>
2021-10-01 13:41:18 +0700

    behavioral design pattern git push

--- module2/src/_20_behavioral_design_pattern/practice/observer/Observer.java
+++ module2/src/_20_behavioral_design_pattern/practice/observer/Observer.java
@@ -1,8 +1,8 @@
 package _20_behavioral_design_pattern.practice.observer;
 
-import javax.security.auth.Subject;
 
 public abstract class Observer {
     protected Subject subject;
     public abstract void update();
+
 }
