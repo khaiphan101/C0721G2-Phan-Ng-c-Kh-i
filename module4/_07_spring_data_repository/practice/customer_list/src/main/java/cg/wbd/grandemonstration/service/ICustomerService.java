b3a4e78908c29e4870b37756800bd0d2d95bcd52
khaiphan101 <khaiphanngoc@gmail.com>
2021-11-24 11:18:57 +0700

    update quan ly tinh + phan trang

--- module4/_07_spring_data_repository/practice/customer_list/src/main/java/cg/wbd/grandemonstration/service/ICustomerService.java
+++
@@ -1,6 +0,0 @@
-package cg.wbd.grandemonstration.service;
-
-import cg.wbd.grandemonstration.model.Customer;
-
-public interface ICustomerService extends IGeneralService<Customer> {
-}
---
+++ module4/_07_spring_data_repository/practice/customer_list/src/main/java/com/codegym/cms/repository/IProvinceRepository.java
@@ -0,0 +1,11 @@
+package com.codegym.cms.repository;
+
+import com.codegym.cms.model.Province;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.expression.spel.ast.Literal;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProvinceRepository extends PagingAndSortingRepository<Province, Long> {
+
+}
