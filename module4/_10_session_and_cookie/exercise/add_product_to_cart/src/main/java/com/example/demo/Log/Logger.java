06316f0327865d57f07c1793689bc2a840f48c91
khaiphan101 <khaiphanngoc@gmail.com>
2021-11-26 02:06:05 +0700

    ud exercise

---
+++ module4/_10_session_and_cookie/exercise/add_product_to_cart/src/main/java/com/example/demo/Log/Logger.java
@@ -0,0 +1,32 @@
+package com.example.demo.Log;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class Logger {
+
+//    @AfterReturning("execution(public * com.example.demo.service.BookService.findAll())")
+//    public void log(JoinPoint joinPoint, Exception e) {
+//        String className = joinPoint.getTarget().getClass().getSimpleName();
+//        String method = joinPoint.getSignature().getName();
+//        String args = Arrays.toString(joinPoint.getArgs());
+//        System.out.println(String.format("[CMS] co loi xay ra: %s.%s%s: %s", className, method, args, e.getMessage()));
+//    }
+
+//    @AfterThrowing(pointcut = "execution(public * com.example.demo.service.BookService.*(..))",
+//            throwing = "e")
+//    public void logError(JoinPoint joinPoint, Exception e) {
+//        String className = joinPoint.getTarget().getClass().getSimpleName();
+//        String method = joinPoint.getSignature().getName();
+//        String args = Arrays.toString(joinPoint.getArgs());
+//        System.out.println(String.format("[CMS] co loi xay ra: %s.%s%s: %s", className, method, args, e.getMessage()));
+//    }
+}
