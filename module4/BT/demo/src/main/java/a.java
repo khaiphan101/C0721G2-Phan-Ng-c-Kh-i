753cc360d780d6fee196da485faaa68955f46ac8
khaiphan101 <88071022+khaiphan101@users.noreply.github.com>
2022-01-09 11:48:53 +0700

    a

--- module4/BT/demo/src/main/java/a.java
+++ module4/BT/demo/src/main/java/a.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+
 
 public class a {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 4, 0, 0, 3, 2, 1, 3, 0, 3};
+        getElement(a);
+
+    }
+
+    private static void getElement(int[] a) {
+        int b = 0,max=0;
+        for (int i = 0; i<a.length-1;i++){
+            for (int j = i+1; j<a.length-1;j++){
+                if (a[i]==a[j]){
+                        b++;
+                }
+            }
+            if (b>max){
+                max = b;
+            }
+        }
+        System.out.println(max);
+    }
+
 }
--- module4/BT/untitled/src/a.java
+++ module4/BT/untitled/src/a.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class a {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 4, 0, 0, 3, 2, 1, 3, 0, 3};
+        maxRepeatingElementInPlace(a);
+        char[] c = new char[]
+    }
+
+
+    public static void maxRepeatingElementInPlace(int[] a) {
+        int size = a.length;
+        int maxNumber = 0;
+        for (int i =0; i< size-1; i++){
+            int temp = 1;
+            for (int j = i+1; j < size-1; j++){
+                if (a[i] == a[j]){
+                    temp++;
+                }
+            }
+            if(temp>maxNumber){
+                maxNumber == temp;
+            }
+        }
+    }
 }
